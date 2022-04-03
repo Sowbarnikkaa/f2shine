656277b02f20e67579448b05bb65bd18192d7cbf
Sowbarnikkaa <91717903+Sowbarnikkaa@users.noreply.github.com>
2022-04-03 21:24:44 +0530

    code update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var MenuItems = document.getElementById("MenuItems");
+var menuItems = document.getElementById("menuItems");
 
-MenuItems.style.maxHeight = "0px";
+menuItems.style.maxHeight = "0px";
 
 function menutoggle() {
-    if (MenuItems.style.maxHeight == "0px") {
-        MenuItems.style.maxHeight = "200px"
+    if (menuItems.style.maxHeight == "0px") {
+        menuItems.style.maxHeight = "200px"
     } else {
-        MenuItems.style.maxHeight = "0px";
+        menuItems.style.maxHeight = "0px";
     }
 }
